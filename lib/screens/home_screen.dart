da6d131f9c8b8bcd7b495a958216667e45ba6ab5
antoniojesus1999 <antoniojesuspv99@gmail.com>
2022-08-21 00:34:19 +0200

    paso a windows nuevo proyecto peliculas

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:peliculas/widgets/card_swiper.dart';
+import 'package:peliculas/widgets/widgets.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text(
+            'Películas en cines',
+          ),
+          elevation: 0,
+          actions: [
+            IconButton(onPressed: () {}, icon: Icon(Icons.search_outlined))
+          ],
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              CardSwiper(),
+              const MovieSlider(),
+              const MovieSlider(),
+              const MovieSlider()
+            ],
+          ),
+        ));
+  }
+}
